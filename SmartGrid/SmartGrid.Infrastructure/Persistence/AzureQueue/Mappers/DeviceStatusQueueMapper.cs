24012a80ee93980c41aedce11d0a326ed9d301e3
furtiwile <furtiwile@gmail.com>
2026-04-17 14:56:47 +0200

    push base project (is not final yet) - stuff still needs to be deleted/removed - there exists a chance that a newer version will be pushed

---
+++ SmartGrid/SmartGrid.Infrastructure/Persistence/AzureQueue/Mappers/DeviceStatusQueueMapper.cs
@@ -0,0 +1,24 @@
+﻿using SmartGrid.Domain.Models;
+using SmartGrid.Infrastructure.Persistence.AzureQueue.Messages;
+
+namespace SmartGrid.Infrastructure.Persistence.AzureQueue.Mappers
+{
+    public static class DeviceStatusQueueMapper
+    {
+        public static DeviceStatusMessage? ToQueueMessage(this DeviceStatus model)
+        {
+            if (model == null) return null;
+
+            return new DeviceStatusMessage
+            {
+                // TODO
+            };
+        }
+
+        public static DeviceStatus? ToDomainModel(this DeviceStatusMessage message)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
