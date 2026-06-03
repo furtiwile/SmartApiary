35a1066853eeec2495ae4e64baf5d6aaec553e7f
acsmandic99 <122286274+acsmandic99@users.noreply.github.com>
2026-06-03 18:59:09 +0200

    implementirana detekcija anomalija za kradju meda

--- SmartApiary/SmartApiary.Application/Interfaces/Repositories/IHiveRepository.cs
+++ SmartApiary/SmartApiary.Application/Interfaces/Repositories/IHiveRepository.cs
@@ -7,6 +7,7 @@ namespace SmartApiary.Application.Interfaces.Repositories
     {
         Task<Hive?> GetByIdAsync(EntityId hiveId, CancellationToken ct = default);
         Task<Hive?> GetByIdAsync(EntityId apiaryId, EntityId hiveId, CancellationToken ct = default);
+        Task<Hive?> GetBySmartScaleIdAsync(EntityId smartScaleId, CancellationToken ct = default);
         Task<IReadOnlyCollection<Hive>> GetByApiaryIdAsync(EntityId apiaryId, CancellationToken ct = default);
         Task SaveAsync(Hive hive, CancellationToken ct = default);
         Task UpdateAsync(Hive hive, CancellationToken ct = default);
---
+++ SmartApiary/SmartApiary.Infrastructure/Services/QueueInitializerHostedService.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SmartApiary.Infrastructure.Common.Options;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartApiary.Infrastructure.Services
+{
+    internal sealed class QueueInitializerHostedService(
+        QueueServiceClient queueServiceClient,
+        IOptions<AzureQueueOptions> options,
+        ILogger<QueueInitializerHostedService> logger
+    ) : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var queueNames = new[]
+            {
+                options.Value.TelemetryQueue,
+                options.Value.AlertQueue,
+                options.Value.DeviceStatusQueue
+            };
+
+            foreach (var queueName in queueNames)
+            {
+                if (!string.IsNullOrWhiteSpace(queueName))
+                {
+                    var queueClient = queueServiceClient.GetQueueClient(queueName);
+                    await queueClient.CreateIfNotExistsAsync(cancellationToken: cancellationToken);
+                    logger.LogInformation("Ensured Azure Queue exists: {QueueName}", queueName);
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
