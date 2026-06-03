de43db6ec62cf7761e17e97f23f00331b306f7bb
acsmandic99 <122286274+acsmandic99@users.noreply.github.com>
2026-06-03 20:59:32 +0200

    implementirao povezivanje sa openWeather i najavu prskanja,popravio izostanak dda nema provere da li parcela postoji pri doddavanju prskanja

---
+++ SmartApiary/SmartApiary.Application/Common/Options/WeatherOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartApiary.Infrastructure.Common.Options
+{
+    /// <summary>
+    /// </summary>
+    public class WeatherOptions
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string BaseUrl { get; set; } = "https://api.openweathermap.org/data/2.5/";
+    }
+}
