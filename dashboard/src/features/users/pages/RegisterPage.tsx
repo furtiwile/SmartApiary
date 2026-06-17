4cb0915cc026e4903a87326b79c4618cfebb80d6
Nikola-Mircic <nikola123mircic@gmail.com>
2026-06-18 01:26:46 +0200

    Updated register button

--- dashboard/src/features/users/pages/RegisterPage.tsx
+++ dashboard/src/features/users/pages/RegisterPage.tsx
@@ -242,7 +242,7 @@ export function RegisterPage() {
               <Link to="/login" className="font-semibold text-indigo-400 hover:text-indigo-300">
                 Already have an account? Log in instead
               </Link>
-              <button type="submit" className="flex w-50 justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-black hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              <button type="submit" className="flex w-50 justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                 Register
               </button>
             </div>
