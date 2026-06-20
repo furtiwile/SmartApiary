476778ebbefcdf80d04490da29f7dae2605f2701
RD4 <rdakic@protonmail.com>
2026-06-20 19:00:52 +0200

    quick package fix, import fix

--- dashboard/src/features/beehives/components/BeehiveList.tsx
+++ dashboard/src/features/beehives/components/BeehiveList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Beehive } from "../models/Beehive";
 import { BeehiveRow } from "./BeehiveRow";
 
--- dashboard/src/features/beehives/pages/BeehivesPage.tsx
+++ dashboard/src/features/beehives/pages/BeehivesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { PageLayout } from "../../../layouts/PageLayout";
 import type { Beehive } from "../models/Beehive";
