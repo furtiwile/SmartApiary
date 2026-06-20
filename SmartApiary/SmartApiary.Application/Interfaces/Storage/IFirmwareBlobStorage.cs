c6431052928e8343d9a4fa962ade5b2db9e0ccfb
furtiwile <furtiwile@gmail.com>
2026-06-20 14:00:56 +0200

    Update API base URLs and remove deprecated firmware handling code

--- SmartApiary/SmartApiary.Application/Interfaces/Storage/IFirmwareBlobStorage.cs
+++
@@ -1,7 +0,0 @@
-﻿using SmartApiary.Application.Common;
-
-namespace SmartApiary.Application.Interfaces.Storage
-{
-    public interface IFirmwareBlobStorage : IFileStorage<FirmwareMetadata>
-    { }
-}
