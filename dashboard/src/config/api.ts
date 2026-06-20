03896a91f0c8887b26cc96766fddf687f9165e3c
Geeky Lynx <73647896+geeky-lynx@users.noreply.github.com>
2026-06-20 23:48:18 +0200

    dashboard/config: Fix Unauthorized errors even when logged in

--- dashboard/src/config/api.ts
+++ dashboard/src/config/api.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
 import { CONFIG } from "./config";
+import { LocalStorage } from "../features/users/helpers/localStorageHelper";
 
 const api = axios.create({
   baseURL: CONFIG.API_BASE_URL,
 });
+
+// This should fix 401 (Unauthorized) errors when calling backend APIs
+api.interceptors.request.use(
+  (config) => {
+    const token = LocalStorage.get("authToken");; 
+    if (token)
+      config.headers["Authorization"] = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
