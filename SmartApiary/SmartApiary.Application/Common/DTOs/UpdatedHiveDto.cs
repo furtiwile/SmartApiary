9a998355f28b62c3c7d53bae7602d534e2808845
acsmandic99 <122286274+acsmandic99@users.noreply.github.com>
2026-06-22 15:45:32 +0200

    bugfix update hive

---
+++ SmartApiary/SmartApiary.Application/Common/DTOs/UpdatedHiveDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApiary.Application.Common.DTOs
+{
+    public record UpdatedHiveDto(
+        string Id,
+        string ApiaryId,
+        string Designation,
+        int Type,
+        string SuperColor,
+        int QueenAge,
+        string Note,
+        string? SmartScaleId
+    );
+}
